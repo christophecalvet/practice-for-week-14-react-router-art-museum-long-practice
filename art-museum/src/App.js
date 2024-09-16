8bb974b0e23e6706b6b318f7c8e853ef47a3042b
Christophe Calvet <onlyatest@mytest.com>
2024-09-17 00:03:40 +0200

    part3 done

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -8,10 +8,22 @@ import { NavLink, Route, Switch, useParams } from 'react-router-dom';
 function App() {
   return (
     <div className="page-wrapper">
+
     <GalleryNavigation galleries={harvardArt.records}/>
-    <Route path="/galleries/:galleryId">
-    <GalleryView galleries={harvardArt.records} />
-    </Route>
+
+    <Switch>
+      <Route exact path="/">
+      <h2>Harvard Art Museum</h2>
+      <p>Look, but Don't Touch. Please select a Gallery in the navigation bar.</p>
+      </Route>
+      <Route path="/galleries/:galleryId">
+      <GalleryView galleries={harvardArt.records} />
+      </Route>
+      <Route>
+      <h2>Page not found</h2>
+      </Route>
+    </Switch>
+
     </div>
   );
 }
